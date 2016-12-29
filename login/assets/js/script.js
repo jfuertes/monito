4d884cd64a940c846be1098f6bfbff72d57169a5
aleluis777 <luislinaresburnes@gmail.com>
2016-12-29 17:55:43 -0500

    ultimo casa jair juebez

--- login/assets/js/script.js
+++ login/assets/js/script.js
@@ -10,14 +10,14 @@ $routeProvider
 
             // route for the home page
             .when('/', {
-                templateUrl : 'pages/home.html',
+                templateUrl : 'pages/login.html',
                 controller  : 'mainCtrl'
             })
 
             // route for the about page
-            .when('/alumno', {
-                templateUrl : 'pages/alumno.html',
-                controller  : 'alumnoCtrl'
+            .when('/formulario', {
+                templateUrl : 'pages/formulario.html',
+                controller  : 'formularioCtrl'
             })
 
             // route for the contact page
@@ -51,79 +51,7 @@ loginApp.controller('mainCtrl', function($scope) {
         $scope.message = 'Hello world, this is the home page!';
     });
 
-loginApp.controller('alumnoCtrl', function($scope, $http, $rootScope) {
 
-           $scope.cursos= function(id){
-            
-            $rootScope.nivel=id;
-           window.location.href='#cursos';
-            console.log($rootScope.nivel);
-      }
-
-
-        $scope.message = 'Hi! This is the about page.';
-         $scope.getNivel= function(){
-             $http.post('api/getNivel.php' )
-                .success(function(data) {
-                  console.log(data);
-                  $scope.Nivel=data;
-                })
-                .error(function(data) {
-                  console.log('Error: ' + data);
-                  });
-         };
-         $scope.getNivel();
-
-    });
-
-loginApp.controller('listaProfCtrl', function($scope) {
-  $scope.message = 'Would you like to contact us?';
-    });
-
-
-
-loginApp.controller('temasCtrl', function($scope) {
-  $scope.message = 'Would you like to contact us?';
-    });
-
-
-loginApp.controller('cursosCtrl', function($scope, $http, $rootScope) {
-        $scope.cursos= function(){
-            window.location.href='#';
-            console.log("entra en la consola scope boton1");
-      };
-      $scope.ejecutar=function(id, seccion){
-            if(seccion=="0"){
-                    alert(id);
-                     $http.post('api/getSubcursos.php',{id:id} )
-                    .success(function(data) {
-                      console.log(data);
-                      $scope.Subcursos=data;
-                    })
-                    .error(function(data) {
-                      console.log('Error: ' + data);
-                      });
-
-                 $scope.VerSubcursos=true;
-            }
-              if(seccion!="0"){
-                window.location.href='#metodo';
-            }
-
-      };
-       $scope.getCursos= function(){
-             $http.post('api/getCursosByNivel.php',{nivel:$rootScope.nivel} )
-                .success(function(data) {
-                  console.log(data);
-                  $scope.Cursos=data;
-                })
-                .error(function(data) {
-                  console.log('Error: ' + data);
-                  });
-         };
-         $scope.getCursos();
-    });
-
-loginApp.controller('metodoCtrl', function($scope) {
+loginApp.controller('formularioCtrl', function($scope) {
   $scope.message = 'Would you like to contact us?';
     });
