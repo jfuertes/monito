5bc7fd9befdca46f02e8758200c4df794de00f6e
aleluis777 <luislinaresburnes@gmail.com>
2017-03-21 23:38:19 -0500

    ordenamiento2

--- monito2.php
+++ monito2.php
@@ -30,8 +30,25 @@
     <style type="text/css"></style>
     <style  href="assets/css/miprofe.css" type="text/css"></style>
     </head>
-    <body class="global-es logged-out splash-2014 compact-enabled" style="overflow: auto;">
-    <header ng-controller="mainCtrl" class="topbar topbar-blue-dark">
+<body class="global-es logged-out splash-2014 compact-enabled" style="overflow: auto;">
+    <div id="fb-root" class=" fb_reset fb_reset fb_reset">
+        <div style="position: absolute; top: -10000px; height: 0px; width: 0px;">
+            <div>
+                <iframe name="fb_xdm_frame_https" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" id="fb_xdm_frame_https" aria-hidden="true" title="Facebook Cross Domain Communication Frame" tabindex="-1" src="https://staticxx.facebook.com/connect/xd_arbiter/r/1FegrZjPbq3.js?version=42#channel=f230bca32c29598&amp;origin=https%3A%2F%2Fwww.duolingo.com" style="border: none;"></iframe>
+            </div>
+        </div>
+        <div style="position: absolute; top: -10000px; height: 0px; width: 0px;">
+            <div></div>
+        </div>
+        <div style="position: absolute; top: -10000px; height: 0px; width: 0px;">
+            <div></div>
+        </div>
+        <div style="position: absolute; top: -10000px; height: 0px; width: 0px;">
+            <div></div>
+        </div>
+    </div>
+    <div id="topbar">
+        <header ng-controller="mainCtrl" class="topbar topbar-blue-dark">
         <div class="container">
         <a href="#/" class="topbar-brand navigate-home track-click white smaller-logo"> <img src="IMG/logo.png" WIDTH=100 HEIGHT=80></a>
             <div class="topbar-right">
@@ -81,6 +98,15 @@
         </div>
     </header>
 
+<div id="mobile-menu" class="mobile-menu open">
+            <ul class="mobile-menu-listing">
+                <li><a href="javascript:;" class="show-log-in">Ingresar como Profesor</a>
+                </li>
+                <li><a href="/register">Ingresar como Alumno</a>
+                </li>
+                
+            </ul>
+        </div>
     <div id="main">
         <div ng-view>
         </div>
